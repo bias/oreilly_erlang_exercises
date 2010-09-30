9e7fa61d5367b86dc27dfc90eceeecac40f990e3
Trevor <trevor@localhost.local>
2010-09-30 14:11:56 -0500

    testing

--- ch15/peer.erl
+++ ch15/peer.erl
@@ -1,5 +1,5 @@
 -module(peer).
--export([start/0, client/2, send/2, wait_connect/2, get_request/3, handle/2]).
+-export([start/0, client/2, wait_connect/2, get_request/3, handle/2]).
 
 start() ->
 	{ok, ListenSocket} = gen_tcp:listen(1234, [binary, {active, false}]),
@@ -10,12 +10,11 @@ client(Host, Data) ->
     tcp:send(Socket, Data),
     ok = gen_tcp:close(Socket).
 
-wait_connect(ListenSocket,0).
 wait_connect(ListenSocket, Count) ->
 	{ok, Socket} = gen_tcp:accept(ListenSocket),
 	Pid = spawn(?MODULE, get_request, [Socket, [], Count]),
 	gen_tcp:controlling_process(Socket, Pid),
-wait_connect(ListenSocket, Count+1).
+	wait_connect(ListenSocket, Count+1).
 
 
 get_request(Socket, BinaryList, Count) ->
